d172313c7b6225e60a9e772137aae9565c66eb86
chaudhryusman93 <chaudhryusman9362@gmail.com>
2023-10-29 19:55:31 +0500

    push notification

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -3,7 +3,7 @@ import 'package:chatter/pages/RegisterationPage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../modelClasses/ChatUsers.dart';
-import '../drawer/chatPage.dart';
+import 'chatPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
